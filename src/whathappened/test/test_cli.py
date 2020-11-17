1ccf6ec345b23156f28d530dcac4e2c150ca7ad4
Darren Meehan <hi@drn.ie>
2020-11-17 22:24:57 +0000

    Add two tests to check arg parser setup
    Initial sanity checking, will be changed soon enough

--- src/whathappened/test/test_cli.py
+++ src/whathappened/test/test_cli.py
@@ -1,5 +1,13 @@
+import pytest
+
 from whathappened.cli import add_arguments
 
 
-def test_setup():
-    add_arguments()
+def test_sync_required_args_supplied():
+    parser = add_arguments()
+    parser.parse_args(["sync", "--bucket-name", "testing"])
+
+def test_sync_no_args_required():
+    parser = add_arguments()
+    with pytest.raises(SystemExit):
+        parser.parse_args(["sync"])
