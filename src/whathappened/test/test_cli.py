4b6957dca78f9cca0e72ab67074f8c0183665c00
Darren Meehan <hi@drn.ie>
2020-11-19 18:27:14 +0000

    Add failing test for when I come back to cli

--- src/whathappened/test/test_cli.py
+++ src/whathappened/test/test_cli.py
@@ -1,4 +1,5 @@
 import pytest
+import subprocess
 
 from whathappened.cli import add_arguments
 
@@ -12,3 +13,10 @@ def test_sync_no_args_required():
     parser = add_arguments()
     with pytest.raises(SystemExit):
         parser.parse_args(["sync"])
+
+@pytest.mark.xfail()
+def test_command_works_from_shell_exit_zero():
+    """
+    Basic sanity test that the command exits without error.
+    """
+    subprocess.run("whathappened", check=True)
