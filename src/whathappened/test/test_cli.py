2fc466f191de65e9d6511d0284b2195b51c3f73b
Darren Meehan <hi@drn.ie>
2020-11-19 18:34:10 +0000

    Move integration tests to new directory

--- src/whathappened/test/test_cli.py
+++ src/whathappened/test/test_cli.py
@@ -13,10 +13,3 @@ def test_sync_no_args_required():
     parser = add_arguments()
     with pytest.raises(SystemExit):
         parser.parse_args(["sync"])
-
-@pytest.mark.xfail()
-def test_command_works_from_shell_exit_zero():
-    """
-    Basic sanity test that the command exits without error.
-    """
-    subprocess.run("whathappened", check=True)
---
+++ test/test_integration.py
@@ -0,0 +1,10 @@
+import subprocess
+
+import pytest
+
+@pytest.mark.xfail()
+def test_command_works_from_shell_exit_zero():
+    """
+    Basic sanity test that the command exits without error.
+    """
+    subprocess.run("whathappened", check=True)
