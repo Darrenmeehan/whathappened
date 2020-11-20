c8d6ec62b99df6f9a7d847c7560c2a6c7bed09ee
Darren Meehan <hi@drn.ie>
2020-11-20 16:25:16 +0000

    Mock out AWS creds for unit tests

--- src/whathappened/test/conftest.py
+++ src/whathappened/test/conftest.py
@@ -4,3 +4,15 @@ import pytest
 @pytest.fixture()
 def correct_commandline_arguments():
     return ["sync,", "--bucket-name", "hello"]
+
+
+@pytest.fixture(autouse=True)
+def mock_aws_credentials(monkeypatch):
+    """
+    Mock out AWS credentials.
+
+    Tests should not reach out to the Internet.
+    But they inevitably will. This will stop anything from happening.
+    """
+    monkeypatch.setenv("AWS_ACCESS_KEY_ID", "AWS_ACCESS_KEY_ID")
+    monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "AWS_SECRET_ACCESS_KEY")
