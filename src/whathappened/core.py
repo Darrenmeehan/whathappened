be15099fb2edd0b91a5a338ca5058b868620db76
Darren Meehan <hi@drn.ie>
2020-11-20 16:24:57 +0000

    Add required import

--- src/whathappened/core.py
+++ src/whathappened/core.py
@@ -6,6 +6,7 @@ https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-
 
 import sys
 
+from whathappened.aws_fun import sync_bucket
 from whathappened.cli import add_arguments
 
 
